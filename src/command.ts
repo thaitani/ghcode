33e24e1dbf212aea086cee4c7c05210042c3e173
thaitani <42672817+thaitani@users.noreply.github.com>
2023-03-10 10:15:08 +0900

    refactor(command): PATH整理

--- src/command.ts
+++ src/command.ts
@@ -3,7 +3,7 @@ import { execSync } from "child_process";
 const execSyncWrap = (command: string): string => {
   try {
     return execSync(command, {
-      env: { ...process.env, PATH: "/opt/homebrew/bin/:/usr/gnu/bin:/usr/local/bin:/bin:/usr/bin:." },
+      env: { ...process.env, PATH: "/opt/homebrew/bin/:/usr/bin:/bin" },
     }).toString();
   } catch (e) {
     throw new Error(`command failed: ${command}`);
