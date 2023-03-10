be2ff20f438cd9efaa6e0335dbbe6d128cd22422
thaitani <42672817+thaitani@users.noreply.github.com>
2023-03-10 10:13:05 +0900

    refactor(command.ts): process.envを上書きしていたところを追記に修正

--- src/command.ts
+++ src/command.ts
@@ -3,13 +3,9 @@ import { execSync } from "child_process";
 const execSyncWrap = (command: string): string => {
   try {
     return execSync(command, {
-      env: {
-        HOME: "/Users/toshiki",
-        PATH: "/opt/homebrew/bin/:/usr/gnu/bin:/usr/local/bin:/bin:/usr/bin:.",
-      },
+      env: { ...process.env, PATH: "/opt/homebrew/bin/:/usr/gnu/bin:/usr/local/bin:/bin:/usr/bin:." },
     }).toString();
   } catch (e) {
-    console.log(e);
     throw new Error(`command failed: ${command}`);
   }
 };
