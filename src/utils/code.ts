14d982bd157cfe2bd435c9468b846b46b13c0095
thaitani <42672817+thaitani@users.noreply.github.com>
2023-03-11 13:39:19 +0900

    fix(code): codeのエラー処理追加

--- src/utils/code.ts
+++ src/utils/code.ts
@@ -1,3 +1,14 @@
+import { Toast, showToast } from "@raycast/api";
 import { execSyncWrap } from "./command";
 
-export const openWithCode = (path: string) => execSyncWrap(`code ${path}`);
+export const openWithCode = (path: string) => {
+  try {
+    execSyncWrap(`code ${path}`);
+  } catch (e) {
+    if (e instanceof Error) {
+      showToast({ title: e.name, style: Toast.Style.Failure, message: e.message });
+    } else {
+      showToast({ title: "Unknown Error", style: Toast.Style.Failure });
+    }
+  }
+};
